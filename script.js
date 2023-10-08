8ce26709f77db046de856efcef613717a6228e4f
VishrutiBuddhadev <vishrutibuddhadev1405@gmail.com>
2023-10-08 12:11:53 +0530

    MyPortfolio

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var typed = new Typed('#element', {
-    strings: ['<i></i> I am a Computer Science Graduate.'],
-    typeSpeed: 50,
-  });
-  
+  strings: ['<i></i> I am a Computer Science Graduate.'],
+  typeSpeed: 50,
+});
+
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navbarSupportedContent')
 const bsCollapse = new bootstrap.Collapse(menuToggle)
 navLinks.forEach((l) => {
-    l.addEventListener('click', () => { bsCollapse.toggle() })
+  l.addEventListener('click', () => { bsCollapse.toggle() })
 })
